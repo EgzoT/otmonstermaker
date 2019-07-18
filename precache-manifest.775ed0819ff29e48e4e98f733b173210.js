d4be078efac645954480457ae89eaed1fdda3b17
Egzo <przemek7575@prokonto.pl>
2019-07-18 20:41:34 +0200

    Add gh-pages app

---
+++ precache-manifest.775ed0819ff29e48e4e98f733b173210.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ebeb3d6e8245bbaffda189ceae1951",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3a3ba0efa4ec44241bb",
+    "url": "/static/css/main.90ee9581.chunk.css"
+  },
+  {
+    "revision": "e4d73add9f1947cdd491",
+    "url": "/static/js/2.7200fb1d.chunk.js"
+  },
+  {
+    "revision": "f3a3ba0efa4ec44241bb",
+    "url": "/static/js/main.874bc728.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
