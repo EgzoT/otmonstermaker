de6f7d8726ae3f5097dbcc8fd1ec31bfd68d9151
Egzo <przemek7575@prokonto.pl>
2019-08-10 12:23:46 +0200

    Update to commit #43250ee

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.775ed0819ff29e48e4e98f733b173210.js"
+  "/precache-manifest.a4f88ef998583f63e8fb10a0a1e33f47.js"
 );
 
 self.addEventListener('message', (event) => {
